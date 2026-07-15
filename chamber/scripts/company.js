2dc2dad89995d087cac7150dc2d882293b7bd1d4
iloantsa <ton@email.com>
2026-07-15 19:27:41 +0200

    week 2 assignment

---
+++ chamber/scripts/company.js
@@ -0,0 +1,68 @@
+{
+    "companies": [
+        {
+            "name": "Joseph",
+            "adress": "Smith",
+            "email":"info1@gail.com",
+            "phonenumber": "23 December 1805",
+            "weburl": "231 34 13 533 44",
+            "membershiplevel": "1 = member, 2= silver, 3= gold",
+            "image": "https://byui-cse.github.io/cse-ww-program/data/images/prophets/joseph-smith.jpg"
+        },
+        {
+            "name": "Joseph",
+            "adress": "Smith",
+            "email": "info2@gail.com",
+            "phonenumber": "231 34 13 533 43",
+            "weburl": "27 June 1844",
+            "membershiplevel": 14,
+            "image": "https://byui-cse.g
+
+        },
+        {
+            "name": "Joseph",
+            "adress": "Smith",
+            "email": "info3@gail.com",
+            "phonenumber": "231 34 13 533 42",
+            "weburl": "27 June 1844",
+            "membershiplevel": 14,
+            "image": "https://byui-cse.g
+        },
+        {
+            "name": "Joseph",
+            "adress": "Smith",
+            "email":"info4@gail.com",
+            "phonenumber": "231 34 13 533 41",
+            "weburl": "27 June 1844",
+            "membershiplevel": 14,
+            "image": "https://byui-cse.g
+        },
+        {
+            "name": "Joseph",
+            "adress": "Smith",
+            "email": "info5@gail.com",
+            "phonenumber": "231 34 13 533 40",
+            "weburl": "27 June 1844",
+            "membershiplevel": 14,
+
+            "image": "https://byui-cse.gse.github.io/cse-ww-program/data/images/prophets/lorenzo-snow.jpg"
+        },
+        {
+            "name": "Joseph",
+            "adress": "Smith",
+            "email": "info6@gail.com",
+            "phonenumber": "231 34 13 533 49",
+            "weburl": "27 June 1844",
+            "membershiplevel": 14,
+            "image": "https://byui-cse.g.io/cse-ww-program/data/images/prophets/joseph-f-smith.jpg"
+        },
+        {
+            "name": "Joseph",
+            "adress": "Smith",
+            "email": "info7@gail.com",
+            "phonenumber": "231 34 13 533 47",
+            "weburl": "27 June 1844",
+            "membershiplevel": 14,
+            "image": "https://byui-cse.g
+        },
+    }        
